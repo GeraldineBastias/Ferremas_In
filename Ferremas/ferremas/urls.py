41a7cc6fbdc797ed672d8774f69491fbeb59f9b5
FlavioOpazo <fl.opazo@duocuc.cl>
2024-05-20 05:03:46 -0400

    con errores

--- Ferremas/ferremas/urls.py
+++ Ferremas/ferremas/urls.py
@@ -1,7 +1,7 @@
-"""ferremas URL Configuration
+"""djangocrud URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tasks import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('tasks_completed/', views.tasks_completed, name='tasks_completed'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+    path('create_task/', views.create_task, name='create_task'),
+    path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
+    path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
+    path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
 ]
